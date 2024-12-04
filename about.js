59e9da7f88bdeb902aba8cca6ece8444456c5c11
BugHunter-X <askzenai@gmail.com>
2024-12-04 12:54:06 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,28 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app!</h1>
+      <Link href="/about">
+        <a>Go to About Page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error because `fetch` is not allowed here.
+  const data = await fetch('/api/data');
+  const json = await data.json();
+
+  return (
+    <div>About Page</div>
+  );
+}
+```
